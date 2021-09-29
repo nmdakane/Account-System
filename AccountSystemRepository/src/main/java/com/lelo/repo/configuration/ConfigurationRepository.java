b199b69d691679bba8a1301f838ff6153a986d3c
Nompumelelo Mdakane <lelolee.mdakane18@gmail.com>
2021-09-29 23:45:25 +0200

    modules linked update

---
+++ AccountSystemRepository/src/main/java/com/lelo/repo/configuration/ConfigurationRepository.java
@@ -0,0 +1,8 @@
+package com.lelo.repo.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationRepository {
+}
---
+++ AccountSystemWebServices/src/main/java/com/lelo/services/MainRestApplication.java
@@ -0,0 +1,11 @@
+package com.lelo.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainRestApplication.class, args);
+    }
+}
