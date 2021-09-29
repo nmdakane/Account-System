735d28b7e203af50566ccc4395c1c283290bf567
Nompumelelo Mdakane <lelolee.mdakane18@gmail.com>
2021-09-29 16:31:35 +0200

    first version commit

---
+++ src/main/java/com/lelo/main/MainRestApplication.java
@@ -0,0 +1,12 @@
+package com.lelo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MainRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainRestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lelo/main/config/webconfig.java
@@ -0,0 +1,7 @@
+package com.lelo.main.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class webconfig {
+}
